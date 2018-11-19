f313daba0f537f980f4546a941dbe99fca63456b
Juan Andrés Rivera <jrivera.v@outlook.com>
2018-11-19 18:28:29 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-
-
-
 exports.hideEmail = function  (email) {
     if( validateEmail(email)){
-         
         let limit = email.indexOf('@'); 
         let start = Math.round(limit/3);   
         let hideEmail = email.split("")
@@ -18,7 +14,7 @@ exports.hideEmail = function  (email) {
 function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
-  }
+}
 
 
 
